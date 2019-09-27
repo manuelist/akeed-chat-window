0c62d713e6023acf30821e6d4d835735329de62b
DESKTOP-KMQGR15\manuelist <wilver@leandevinc.com>
2019-09-27 23:46:08 +0800

    Add Initial Files

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'akeed-care'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        Sample
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
