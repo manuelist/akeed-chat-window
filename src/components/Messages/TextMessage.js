1e8c90a563dbcc912891ae26ffb5767f0d7d167d
DESKTOP-KMQGR15\manuelist <wilver@leandevinc.com>
2019-10-18 22:49:26 +0800

    Fix html rendering

--- src/components/Messages/TextMessage.js
+++ src/components/Messages/TextMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 const TextMessage = props => {
   return (
     <div className="sc-message--text" data-id={props.id}>
-      <span>{props.data.text}</span>
+      <span dangerouslySetInnerHTML={{ __html: props.data.text }} />
       <span className="sc-timestamp">{props.dateCreated}</span>
     </div>
   );
